1790e3f2a47030cef072fc1d244ddcc4773b5f24
jvmcpheron <janemcpheron@gmail.com>
2023-11-17 16:06:40 -0600

    added tests

--- main.cpp
+++ main.cpp
@@ -2,13 +2,54 @@
 
 /*
 
+TEST ONE
+./main
+Add node:100
+Add node:68
+Add node:50
+Add node:90
+Add node:567
+Add node:0
+Add node:1
+Add node:89
+Add node:1070
+Add node:1015
+Add node:1000
+Add node:666
+Add node:1111
+Add node:2000
+Add node:;
+Numbers between 5 and 15: 
+Numbers between 1000 and 1010: 
+Numbers between 1020 and 1010: 
+Numbers between 0 and 1: 
+Numbers between 20 and 100: 50 68 89 90 
+
+
+TEST TWO
+./main
+Add node:6
+Add node:18
+Add node:1005
+Add node:15
+Add node:1010
+Add node:5
+Add node:7
+Add node:555
+Add node:123
+Add node:;
+Numbers between 5 and 15: 6 7 
+Numbers between 1000 and 1010: 1005 
+Numbers between 1020 and 1010: 
+Numbers between 0 and 1: 
+Numbers between 20 and 100: 
 
 */
 
 #include <iostream>
 #include <vector>
 
-namespace std{
+using namespace std;
 
     //node class
     class BinaryNode{
@@ -44,7 +85,39 @@ namespace std{
             BinaryNode* getRoot() { return root; }
             bool isEmpty() { return (root == nullptr); }
             BinaryNode *find(int d);
-            void insert(int d);
+
+            //added insert method
+            void insert(int d){ 
+                if (isEmpty()) {
+                    root = new BinaryNode(d);
+                } else {
+                    BinaryNode* current = root;
+                    bool inserted = false;
+
+                    while (!inserted) {
+                        if (d < current->getData()) {
+                            if (current->getLeft() == nullptr) {
+                                current->setLeft(new BinaryNode(d));
+                                inserted = true;
+                            } else {
+                                current = current->getLeft();
+                            }
+                        } else if (d > current->getData()) {
+                            if (current->getRight() == nullptr) {
+                                current->setRight(new BinaryNode(d));
+                                inserted = true;
+                            } else {
+                                current = current->getRight();
+                            }
+                        } else {
+                            // Duplicate values are not allowed
+                            // You can modify this part based on your requirements
+                            inserted = true;
+                        }
+                    }
+                }
+            };
+
             bool remove(int d);
             void printFlat(ostream &out);
             void printInOrder(ostream &out);
@@ -105,13 +178,83 @@ namespace std{
 
     }
 
-};
+
 
 
 
 int main(){
 
+    //TEST
 
+    //test tree
+    BinaryTree testTree = BinaryTree();
+
+    string input = "";
+
+    //input nodes
+    while (input != ";"){
+        cout << "Add node:";
+        cin >> input;
+
+        //stops loop if semicolon
+        if(input == ";"){
+            break;
+        }
+
+        int numIn = stoi(input);
+
+        
+        //adds input to tree
+        testTree.insert(numIn);
+        
+    }
+
+    //now that there is a tree, time to test 
+    vector<int> result1 = betweenTwoNums(testTree, 5, 15);
+
+    //display results
+    cout << "Numbers between 5 and 15: ";
+    for (int num : result1) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+
+    result1 = betweenTwoNums(testTree, 1000, 1010);
+
+    //display results
+    cout << "Numbers between 1000 and 1010: ";
+    for (int num : result1) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    result1 = betweenTwoNums(testTree, 1020, 1010);
+
+    //display results
+    cout << "Numbers between 1020 and 1010: ";
+    for (int num : result1) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    result1 = betweenTwoNums(testTree, 0, 1);
+
+    //display results
+    cout << "Numbers between 0 and 1: ";
+    for (int num : result1) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    result1 = betweenTwoNums(testTree, 20, 100);
+
+    //display results
+    cout << "Numbers between 20 and 100: ";
+    for (int num : result1) {
+        cout << num << " ";
+    }
+    cout << endl;
 
 
 }
